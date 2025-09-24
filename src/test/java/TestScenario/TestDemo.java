8e90b3e02fe1fac3e37efe9644c28317bb25baa8
shreyaumbarkar <shreyaumbarkar2307@gmail.com>
2025-09-24 20:53:01 +0530

    Updated TestDemo.java with new changes

--- src/test/java/TestScenario/TestDemo.java
+++ src/test/java/TestScenario/TestDemo.java
@@ -1,5 +1,12 @@
 package TestScenario;
 
 public class TestDemo {
+    
+    public TestDemo() {
+        System.out.println("Hii");
+    }
 
+    public static void main(String[] args) {
+        new TestDemo(); 
+    }
 }
